35c67e129bcfd2172c81be5331db0c4ba31bb892
Romain Maneschi <romain@maneschi.fr>
2017-02-28 20:43:59 +0100

    add a test with  but nothing generated

--- test/samples/Simple.java
+++ test/samples/Simple.java
@@ -17,4 +17,30 @@ public class Simple {
     public void setNumber(Integer number) {
         this.number = number;
     }
+
+    public static class Builder {
+        private Integer number;
+        private String string;
+
+        public Builder() {
+        }
+
+        public Builder setNumber(Integer number) {
+            this.number = number;
+            return this;
+        }
+
+        public Builder setString(String string) {
+            this.string = string;
+            return this;
+        }
+
+        public Simple build() {
+            Simple simple = new Simple();
+            simple.string = this.string;
+            simple.setNumber(this.number);
+            return simple;
+        }
+
+    }
 }
